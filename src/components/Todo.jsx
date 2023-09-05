4bb8c47cce8c0801230a13dc6516abe2b9c5c577
mustafagarko <keligov.m@mail.ru>
2023-09-05 03:00:49 +0300

    more responsive

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,31 +2,24 @@ import desk from "../assets/desk.avif";
 import note from "../assets/note.webp";
 
 const Todo = function ({ data }) {
-  const chunkedData = [];
-  for (let i = 0; i < data.length; i += 4) {
-    chunkedData.push(data.slice(i, i + 4));
-  }
-
   return (
     <div
-      className="relative  bg-cover z-10 w-screen h-[60.25vw] bg-no-repeat bg-center"
+      className="relative bg-fill md:bg-cover  z-10 w-screen h-[150vh] md:h-[60.25vw] bg-no-repeat bg-center"
       style={{
         backgroundImage: `url(${desk})`,
       }}
     >
-      <div className="absolute top-[10%] w-[70%] z-30 left-[14%]">
-        {chunkedData.map((chunk, rowIndex) => (
-          <div key={rowIndex} className="flex gap-2 md:gap-10">
-            {chunk.map((todo) => (
-              <div key={todo.id} className=" w-[25%] h-[18%] relative z-30">
-                <img src={note} className="w-full h-full " alt="Note" />
-                <p className="text-black absolute md:p-2 md:text-base text-[0.5rem] inset-2 md:inset-4 break-words">
-                  {todo.text}
-                </p>
-              </div>
-            ))}
-          </div>
-        ))}
+      <div className="absolute md:top-[10%] top-[5%] md:w-[73%] w-[80%] z-30 md:left-[14%] left-[5%]">
+        <div className="flex flex-wrap gap-5 md:gap-4">
+          {data.map((todo) => (
+            <div key={todo.id} className="w-[45%] md:w-[23%] relative z-30">
+              <img src={note} className="w-full h-full " alt="Note" />
+              <p className="text-black absolute md:p-2 md:text-base text-[0.5rem] inset-2 md:inset-4 break-words">
+                {todo.text}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
