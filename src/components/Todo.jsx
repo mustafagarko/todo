bfaa5fefd1289f1e4598d826715dee6ccda550d4
mustafagarko <keligov.m@mail.ru>
2023-09-05 03:10:22 +0300

    responsive adjusted

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -10,11 +10,11 @@ const Todo = function ({ data }) {
       }}
     >
       <div className="absolute md:top-[10%] top-[5%] md:w-[73%] w-[80%] z-30 md:left-[14%] left-[5%]">
-        <div className="flex flex-wrap gap-5 md:gap-4">
+        <div className="flex flex-wrap gap-5 md:gap-0">
           {data.map((todo) => (
             <div key={todo.id} className="w-[45%] md:w-[23%] relative z-30">
               <img src={note} className="w-full h-full " alt="Note" />
-              <p className="text-black absolute md:p-2 md:text-base text-[0.5rem] inset-2 md:inset-4 break-words">
+              <p className="text-black absolute md:p-2 md:text-base text-sm inset-5 md:inset-4 break-words">
                 {todo.text}
               </p>
             </div>
